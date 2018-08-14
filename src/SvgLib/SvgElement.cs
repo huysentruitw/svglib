94b31a64fc8b3442a5fb1a529a46da5de90bcb20
Wouter Huysentruit <wouter.huysentruit@sweetmustard.be>
2018-08-14 16:29:04 +0200

    Extend SvgElement and add support for classes and styles

--- src/SvgLib/SvgElement.cs
+++ src/SvgLib/SvgElement.cs
@@ -1,5 +1,6 @@
-﻿using System;
+using System;
 using System.Collections.Generic;
+using System.Linq;
 using System.Xml;
 
 namespace SvgLib
@@ -27,10 +28,46 @@ namespace SvgLib
 
         // TODO Add https://developer.mozilla.org/en-US/docs/Web/SVG/Attribute/Presentation
 
+        public string Fill
+        {
+            get => Element.GetAttribute("fill", SvgDefaults.Attributes.FillAndStroke.Fill);
+            set => Element.SetAttribute("fill", value);
+        }
+
+        public double FillOpacity
+        {
+            get => Element.GetAttribute("fill-opacity", SvgDefaults.Attributes.FillAndStroke.FillOpacity);
+            set => Element.SetAttribute("fill-opacity", value);
+        }
+
         public string Stroke
         {
             get => Element.GetAttribute("stroke", SvgDefaults.Attributes.FillAndStroke.Stroke);
             set => Element.SetAttribute("stroke", value);
+        }
+
+        public double StrokeOpacity
+        {
+            get => Element.GetAttribute("stroke-opacity", SvgDefaults.Attributes.FillAndStroke.StrokeOpacity);
+            set => Element.SetAttribute("stroke-opacity", value);
+        }
+
+        public double StrokeWidth
+        {
+            get => Element.GetAttribute("stroke-width", SvgDefaults.Attributes.FillAndStroke.StrokeWidth);
+            set => Element.SetAttribute("stroke-width", value);
+        }
+
+        public SvgStrokeLineCap StrokeLineCap
+        {
+            get => Element.GetAttribute<SvgStrokeLineCap>("stroke-linecap", SvgDefaults.Attributes.FillAndStroke.StrokeLineCap);
+            set => Element.SetAttribute("stroke-linecap", value);
+        }
+
+        public string Transform
+        {
+            get => Element.GetAttribute("transform");
+            set => Element.SetAttribute("transform", value);
         }
 
         public bool Visible
@@ -39,13 +76,23 @@ namespace SvgLib
             set => SetStyle("display", value ? string.Empty : "none");
         }
 
-        public IEnumerable<string> GetClasses() => throw new NotImplementedException();
+        public IEnumerable<string> GetClasses() => ParseClassAttribute();
 
-        public bool HasClass(string name) => throw new NotImplementedException();
+        public bool HasClass(string name) => GetClasses().Contains(name);
 
-        public void AddClass(string name) => throw new NotImplementedException();
+        public void AddClass(string name)
+        {
+            var classes = ParseClassAttribute();
+            classes.Add(name);
+            SetClassAttribute(classes);
+        }
 
-        public void RemoveClass(string name) => throw new NotImplementedException();
+        public void RemoveClass(string name)
+        {
+            var classes = ParseClassAttribute();
+            classes.Remove(name);
+            SetClassAttribute(classes);
+        }
 
         public void ToggleClass(string name)
         {
@@ -53,14 +100,51 @@ namespace SvgLib
             else AddClass(name);
         }
 
-        private string GetStyle(string name)
+        protected string GetStyle(string name)
         {
-            throw new NotImplementedException();
+            var styles = ParseStyleAttribute();
+            return styles[name];
         }
 
-        private void SetStyle(string name, string value)
+        protected void SetStyle(string name, string value)
         {
-            throw new NotImplementedException();
+            var styles = ParseStyleAttribute();
+            styles[name] = value;
+            SetStyleAttribute(styles);
+        }
+
+        private HashSet<string> ParseClassAttribute()
+            => new HashSet<string>(Element.GetAttribute("class").Split(new[] { ' ' }, StringSplitOptions.RemoveEmptyEntries));
+
+        private void SetClassAttribute(IEnumerable<string> classes)
+        {
+            if (classes == null || !classes.Any())
+            {
+                Element.RemoveAttribute("class");
+                return;
+            }
+
+            var value = string.Join(" ", classes);
+            Element.SetAttribute("class", value);
+        }
+
+        private Dictionary<string, string> ParseStyleAttribute()
+            => Element.GetAttribute("style")
+                .Split(new[] { ';' }, StringSplitOptions.RemoveEmptyEntries)
+                .Select(x => x.Split(new[] { ':' }))
+                .Where(x => x.Length == 2)
+                .ToDictionary(x => x[0].Trim(), x => x[1].Trim(), StringComparer.OrdinalIgnoreCase);
+
+        private void SetStyleAttribute(IReadOnlyDictionary<string, string> styles)
+        {
+            if (styles == null || !styles.Any())
+            {
+                Element.RemoveAttribute("style");
+                return;
+            }
+
+            var value = string.Join(";", styles.Select(kvp => $"{kvp.Key}: {kvp.Value}"));
+            Element.SetAttribute("style", value);
         }
     }
 }
