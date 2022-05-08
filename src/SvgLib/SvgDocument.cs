dac3381c74a73eddec2aba0b44bfb91c8dd5ba98
SFGrenade <25555417+SFGrenade@users.noreply.github.com>
2022-05-08 14:54:08 +0200

    Use SvgDefaults values

--- src/SvgLib/SvgDocument.cs
+++ src/SvgLib/SvgDocument.cs
@@ -26,25 +26,25 @@ namespace SvgLib
 
         public double X
         {
-            get => Element.GetAttribute("x", 0);
+            get => Element.GetAttribute("x", SvgDefaults.Attributes.Position.X);
             set => Element.SetAttribute("x", value);
         }
 
         public double Y
         {
-            get => Element.GetAttribute("y", 0);
+            get => Element.GetAttribute("y", SvgDefaults.Attributes.Position.Y);
             set => Element.SetAttribute("y", value);
         }
 
         public double Width
         {
-            get => Element.GetAttribute("width", 0);
+            get => Element.GetAttribute("width", SvgDefaults.Attributes.Size.Width);
             set => Element.SetAttribute("width", value);
         }
 
         public double Height
         {
-            get => Element.GetAttribute("height", 0);
+            get => Element.GetAttribute("height", SvgDefaults.Attributes.Size.Height);
             set => Element.SetAttribute("height", value);
         }
 
